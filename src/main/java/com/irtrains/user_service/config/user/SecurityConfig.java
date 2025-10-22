f9427bc56beeb9a32151bc6b2f831c2ee7d50473
Aditya Saraswat <adityagaurang2003@gmail.com>
2025-10-22 19:16:03 +0530

    This commit includes improvement in Security.
    In this we remove the certain endpoints from the Security like Login and Register which every user can access. And rest of the services the user will have to Authenticate themselves and than move ahead.

--- src/main/java/com/irtrains/user_service/config/user/SecurityConfig.java
+++ src/main/java/com/irtrains/user_service/config/user/SecurityConfig.java
@@ -1,23 +1,38 @@
 package com.irtrains.user_service.config.user;
 
 import org.springframework.context.annotation.*;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 
 @Configuration
 public class SecurityConfig {
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
 
     @Bean
-    public PasswordEncoder passwordEncoderArg(){
+    public PasswordEncoder passwordEncoder(){
         return new Argon2PasswordEncoder(
             16, 32, 1, 65536, 4
         );
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorize ->
+                authorize.requestMatchers("/api/login","/api/register","/api/trains").permitAll()
+                        .anyRequest().authenticated()
+        )
+                .httpBasic(Customizer.withDefaults())
+                .csrf(csrf -> csrf.disable());
+
+        return http.build();
     }
 }
